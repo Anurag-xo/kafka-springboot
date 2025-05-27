31dabd40fcdc0ffe9d87202f32315cc9d8eff742
Anurag-xo <amnrag889@gmail.com>
2025-05-27 15:38:37 +0000

    Idempotent config

--- src/main/java/com/notrlyanurag/ws/products/KafkaConfig.java
+++ src/main/java/com/notrlyanurag/ws/products/KafkaConfig.java
@@ -36,6 +36,12 @@ public class KafkaConfig {
   @Value("${spring.kafka.producer.producer.request.timeout.ms}")
   private String requestTimeout;
 
+  @Value("${spring.kafka.producer.properties.enable.idempotence}")
+  private boolean idempotence;
+
+  @Value("${spring.kafka.producer.properties.max.in.flight.requests.per.connection}5")
+  private Integer inflightRequests;
+
   Map<String, Object> producerConfigs() {
     Map<String, Object> config = new HashMap<>();
 
@@ -46,6 +52,9 @@ public class KafkaConfig {
     config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
     config.put(ProducerConfig.LINGER_MS_CONFIG, linger);
     config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
+    config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+    config.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, inflightRequests);
+    // config.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
 
     // config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, config);
 
