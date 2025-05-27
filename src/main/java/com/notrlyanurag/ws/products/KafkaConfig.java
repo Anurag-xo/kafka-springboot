c964c9b9e10490fd3b2a0a2fac5086a93391bedd
Anurag-xo <amnrag889@gmail.com>
2025-05-27 12:57:00 +0000

    add producer spring bin configuration

--- src/main/java/com/notrlyanurag/ws/products/KafkaConfig.java
+++ src/main/java/com/notrlyanurag/ws/products/KafkaConfig.java
@@ -1,20 +1,72 @@
 package com.notrlyanurag.ws.products;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
-import java.util.Map;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 public class KafkaConfig {
 
+  @Value("${spring.kafka.producer.bootstrap-servers}")
+  private String bootstrapServers;
+
+  @Value("${spring.kafka.producer.key-serializer}")
+  private String KeySerializer;
+
+  @Value("${spring.kafka.producer.value-serializer}")
+  private String valueSerializer;
+
+  @Value("${spring.kafka.producer.producer.acks}")
+  private String acks;
+
+  @Value("${spring.kafka.producer.producer.properties.delivery.timeout.ms}")
+  private String deliveryTimeout;
+
+  @Value("${spring.kafka.producer.producer.linger.ms}")
+  private String linger;
+
+  @Value("${spring.kafka.producer.producer.request.timeout.ms}")
+  private String requestTimeout;
+
+  Map<String, Object> producerConfigs() {
+    Map<String, Object> config = new HashMap<>();
+
+    config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KeySerializer);
+    config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+    config.put(ProducerConfig.ACKS_CONFIG, acks);
+    config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
+    config.put(ProducerConfig.LINGER_MS_CONFIG, linger);
+    config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
+
+    // config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, config);
+
+    return config;
+  }
+
+  @Bean
+  ProducerFactory<String, ProductCreatedEvent> producerFactory() {
+    return new DefaultKafkaProducerFactory<>(producerConfigs());
+  }
+
+  @Bean
+  KafkaTemplate<String, ProductCreatedEvent> KafkaTemplate() {
+    return new KafkaTemplate<String, ProductCreatedEvent>(producerFactory());
+  }
+
   @Bean
   NewTopic createTopic() {
     return TopicBuilder.name("products-created-events-topic")
-      .partitions(3)
-      .replicas(3)
-      .configs(Map.of("min.insync.replicas","2"))
-      .build();
+        .partitions(3)
+        .replicas(3)
+        .configs(Map.of("min.insync.replicas", "2"))
+        .build();
   }
 }
